849fd66bde1eb0e8064ba1b6d6bce94c22bd49cd
jazz <jazxbx@gmail.com>
2023-03-31 03:03:48 -0700

    update 1

---
+++ project.js
@@ -0,0 +1,42 @@
+"strict";
+
+const playerSelection = prompt("Choose rock, paper, or scissors");
+
+function playRound(playerSelection, getComputerChoice) {
+  if (playerSelection == "rock" && getComputerChoice == "rock") {
+    return "tie";
+  } else if (playerSelection == "rock" && getComputerChoice == "paper") {
+    return "computer wins";
+  } else if (playerSelection == "rock" && getComputerChoice == "scissors") {
+    return "player wins";
+  } else if (playerSelection == "paper" && getComputerChoice == "paper") {
+    return "tie";
+  } else if (playerSelection == "paper" && getComputerChoice == "scissors") {
+    return "computer wins";
+  } else if (playerSelection == "paper" && getComputerChoice == "rock") {
+    return "player wins";
+  } else if (playerSelection == "scissors" && getComputerChoice == "scissors") {
+    return "tie";
+  } else if (playerSelection == "scissors" && getComputerChoice == "rock") {
+    return "computer wins";
+  } else if (playerSelection == "scissors" && getComputerChoice == "paper") {
+    return "player wins";
+  } else {
+    return "try again";
+  }
+}
+
+console.log(playRound(playerSelection, getComputerChoice));
+
+// Random generated response by computer
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+
+  // return this[Math.floor(Math.random() * this.length)];
+}
+
+// console.log(getComputerChoice());
